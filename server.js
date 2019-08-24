a660610a8502c6669566252b9b32231dbf6f2c24
Jarvis Maness <you@example.com>
2019-08-24 09:00:12 -0400

    left off at adding morgan logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
-
-var expressHandlebars = require("express-handlebars");
+//var mongoose = require("mongoose");
+var logger = require("morgan");
+var exphbs = require("express-handlebars");
 
 //Set up our port to be either the host's designated port, or 3000
 var PORT = process.env.PORT || 3000;
@@ -12,20 +13,26 @@ var app = express();
 var router = express.Router();
 
 //Designate our public folder as a static directory
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main"
-}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//Configuring Middleware
+//Use Morgan logger for logging
+app.use(logger("dev"));
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(express.json());
 //Have every request go through our router middleware
 app.use(router);
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"; 
 
+// Connect mongoose to our database
+//mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 //Listen on the port
 app.listen(PORT, function() {
     console.log("Listening on port:" + PORT);
